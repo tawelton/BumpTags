73a4fcbd1b222226f2f4be9d8f7ee1a43f19add7
Tanner Welton <tanner.welton@gmail.com>
2017-08-07 11:34:20 -0600

    Add Workspace

--- Pods/Target Support Files/Alamofire/Alamofire-umbrella.h
+++ Pods/Target Support Files/Alamofire/Alamofire-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_WidgetVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_WidgetVersionString[];
+FOUNDATION_EXPORT double AlamofireVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
 
